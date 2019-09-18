994b46925011900b4a1c1dad70767cc21013dbb1
Nick Altunin <redrolla1@gmail.com>
2019-09-18 20:39:46 +0300

    Transferred #create method into CRuD class Written #update method

--- src/crud/CRuD.java
+++ src/crud/CRuD.java
@@ -1,10 +1,87 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class CRuD {
     private File file;
 
     public CRuD(File file){
         this.file = file;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public void create(String line){
+        //writing new line with maxID + 1 in the end of file;
+        String[] splittedTrafficLine = line.split(";");
+        if(splittedTrafficLine.length != 6){
+            System.out.println("Error, input must be [ship_name;ship_class;current_crew_capacity;tonnage;destination;has_weapons?] without []");
+            System.out.println("Repeat you request please.");
+        }else {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+                String newLine = buildString(maxId() + 1, splittedTrafficLine);
+                if (file.length() != 0) bufferedWriter.write(System.lineSeparator());
+                bufferedWriter.write(newLine);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //updates the String by id
+    public void update(String line){
+        //-u 3;Salvation;Cruiser;00002353;00150000;Eye Of Terror;true
+
+        String[] splittedTrafficLine = line.split(";");
+        if(splittedTrafficLine.length != 7) {
+            System.out.println("Error, input must be [id;ship_name;ship_class;current_crew_capacity;tonnage;destination;has_weapons?] without []");
+            System.out.println("Repeat you request please.");
+        }else {
+            int id = trimZeroesBefore(splittedTrafficLine[0]);
+            splittedTrafficLine = Arrays.copyOfRange(splittedTrafficLine,1,splittedTrafficLine.length);
+            if (idExists(id)){
+                String newLine = buildString(id,splittedTrafficLine);
+                System.out.println(newLine);
+
+                //create .tmp file, copy all contents from original file except line with id, line replaced by update
+                File tempFile = new File(file.getAbsolutePath()+".tmp");
+                try(BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile))){
+
+                    int currentID =-1;
+                    String tempLine = null;
+                    while ((tempLine = bufferedReader.readLine()) != null){
+                        // extracts id from line
+                        if(tempLine.length() > 8){
+                            currentID = trimZeroesBefore(tempLine.split(";")[0]);
+                        }
+
+                        if(currentID != id){
+                            //write the line in temp file
+                            bufferedWriter.write(tempLine);
+                        }else{
+                            //write updated line
+                            bufferedWriter.write(newLine);
+                        }
+                        bufferedWriter.write(System.lineSeparator());
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    //Rename the new file to the filename the original file had.
+                    file.delete();
+                    if (!tempFile.renameTo(file))
+                        System.out.println("Could not rename file");
+
+                }
+            }
+        }
     }
 
     public void remove(int id){
@@ -43,6 +120,64 @@ public class CRuD {
         }
     }
 
+
+    //adds zeros before text to the desirableLength
+    public static String addZerosBefore(String text, int desirableLength){
+        if(text.length() > desirableLength) throw new IllegalArgumentException("Text is longer than desirableLength");
+        if(text.length() == desirableLength) return text;
+
+        text = text.trim();
+        String result = "";
+        for(int i = 0; i < desirableLength-text.length();i++){
+            result+="0";
+        }
+        result+=text;
+        return result;
+    }
+
+    //adds spaces after text to desirableLength
+    public static String addSpacesAfter(String text, int desirableLength){
+        if(text.length() > desirableLength) throw new IllegalArgumentException("Text is longer than desirableLength");
+        if(text.length() == desirableLength) return text;
+
+        text = text.trim();
+        String result = text;
+        for(int i = 0; i < desirableLength-text.length();i++){
+            result+=" ";
+        }
+        return result;
+    }
+
+    //forms up the String in needed format
+    public String buildString(int id, String[] splittedLine){
+        StringBuilder newLine = new StringBuilder();
+                /*
+                id
+                splittedTrafficLine[0] => ship_name
+                splittedTrafficLine[1] => ship_class
+                splittedTrafficLine[2] => current_crew_capacity
+                splittedTrafficLine[3] => tonnage
+                splittedTrafficLine[4] => destination
+                splittedTrafficLine[5] => has_weapons?
+                 */
+        newLine.append(addZerosBefore(String.valueOf(id),8));
+        newLine.append(";");
+        newLine.append(addSpacesAfter(splittedLine[0],32));
+        newLine.append(";");
+        newLine.append(addSpacesAfter(splittedLine[1],24));
+        newLine.append(";");
+        newLine.append(addZerosBefore(splittedLine[2],8));
+        newLine.append(";");
+        newLine.append(addZerosBefore(splittedLine[3],8));
+        newLine.append(";");
+        newLine.append(addSpacesAfter(splittedLine[4],24));
+        newLine.append(";");
+        newLine.append(addSpacesAfter(splittedLine[5],5));
+
+        return newLine.toString();
+    }
+
+
     //checks if id exists in file
     public boolean idExists(int id){
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file)))
@@ -50,8 +185,8 @@ public class CRuD {
             String line;
             while ((line = bufferedReader.readLine()) != null){
                 line = line.split(";")[0];
-                int currentID = trimZeroesBefore(line);
-                if(currentID == id) return true;
+                int currentId = trimZeroesBefore(line);
+                if(currentId == id) return true;
             }
 
         }catch (Exception e){
@@ -60,19 +195,28 @@ public class CRuD {
         return false;
     }
 
+    public int maxId(){
+        int maxId = -1;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file)))
+        {
+            //cycling through file by lines and finding max Id
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                line = line.split(";")[0];
+                int currentId = trimZeroesBefore(line);
+                if(currentId > maxId) maxId = currentId;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return maxId;
+    }
+
     public int trimZeroesBefore(String line){
         while (line.startsWith("0")){
             line = line.replaceFirst("0","");
         }
         int id = Integer.parseInt(line);
         return id;
-    }
-
-    public File getFile() {
-        return file;
-    }
-
-    public void setFile(File file) {
-        this.file = file;
     }
 }
--- src/crud/Main.java
+++ src/crud/Main.java
@@ -13,6 +13,19 @@ public class Main {
         CRuD crud = new CRuD(mainInstance.file);
 
         //input information via console
+        /*
+                    test entries
+                    -c Salvation;Grand cruiser;4353;350000;Eye Of Terror;true
+                    -c Retribution;Grand cruiser;00003456;00350000;Holy Terra    ;true
+                    -c Harrower          ;Firestorm frigate ;00000246;00005367;Ultramar.Calth;true
+                    -c Golden Coin;Privateer frigate;114;6000;Ultramar.Maccrage;false
+
+                    -r 1
+                    -r 3
+
+                    -u 3;Salvation;Cruiser;00002353;00150000;Eye Of Terror;true
+
+         */
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)))
         {
         String line;
@@ -23,11 +36,13 @@ public class Main {
                     System.out.println("Creating new entry.");
                     //TODO check if entry is right?
                     line = line.replaceFirst("-c ","");
-                    mainInstance.create(line,mainInstance.file);
+                    crud.create(line);
                     //if line.split(" ")[0].equals("-c") => call @create
                 }else
                 if(line.startsWith("-u ")){
-                    System.out.println("Updating existing entries.");
+                    System.out.println("Updating existing entry.");
+                    line = line.replaceFirst("-u ","");
+                    crud.update(line);
                 }else
                 if(line.startsWith("-r ")){
                     //-r id   => -r 1
@@ -43,107 +58,5 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    //adds a line to the end of the file with id = max_id + 1;
-    public void create(String trafficLine, File file){
-        int maxID = 0;
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file)))
-        {
-            //cycling through file by lines and finding max ID
-            String line;
-            while ((line = bufferedReader.readLine()) != null){
-                line = line.split(";")[0];
-                //TODO change 3 lines bellow with methom @trimZerosBefore, after @create will be transferred to CRUD class
-                while (line.startsWith("0")){
-                    line = line.replaceFirst("0","");
-                }
-                int currentID = Integer.parseInt(line);
-                if(currentID > maxID) maxID = currentID;
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        //writing new line with maxID + 1 in the end of file;
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))){
-
-            String[] splittedTrafficLine = trafficLine.split(";");
-            if(splittedTrafficLine.length != 6){
-                System.out.println("Error, input must be [ship_name;ship_class;current_crew_capacity;tonnage;destination;has_weapons?] without []");
-                System.out.println("Repeat you request please.");
-            }else{
-                StringBuilder newLine = new StringBuilder();
-
-                /*
-                maxID
-                splittedTrafficLine[0] => ship_name
-                splittedTrafficLine[1] => ship_class
-                splittedTrafficLine[2] => current_crew_capacity
-                splittedTrafficLine[3] => tonnage
-                splittedTrafficLine[4] => destination
-                splittedTrafficLine[5] => has_weapons?
-                 */
-                newLine.append(addZerosBefore(String.valueOf(maxID+1),8));
-                newLine.append(";");
-                newLine.append(addSpacesAfter(splittedTrafficLine[0],32));
-                newLine.append(";");
-                newLine.append(addSpacesAfter(splittedTrafficLine[1],24));
-                newLine.append(";");
-                newLine.append(addZerosBefore(splittedTrafficLine[2],8));
-                newLine.append(";");
-                newLine.append(addZerosBefore(splittedTrafficLine[3],8));
-                newLine.append(";");
-                newLine.append(addSpacesAfter(splittedTrafficLine[4],24));
-                newLine.append(";");
-                newLine.append(addSpacesAfter(splittedTrafficLine[5],5));
-
-                /*
-                    TODO write new line in File
-                    test entries
-                    -c Salvation;Grand cruiser;4353;350000;Eye Of Terror;true
-                    -c Retribution;Grand cruiser;00003456;00350000;Holy Terra    ;true
-                    -c Harrower          ;Firestorm frigate ;00000246;00005367;Ultramar.Calth;true
-                    -c Golden Coin;Privateer frigate;114;6000;Ultramar.Maccrage;false
-
-                 */
-                if(file.length() != 0) bufferedWriter.write(System.lineSeparator());
-                bufferedWriter.write(newLine.toString());
-
-            }
-
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    //adds zeros before text to the desirableLength
-    public static String addZerosBefore(String text, int desirableLength){
-        if(text.length() > desirableLength) throw new IllegalArgumentException("Text is longer than desirableLength");
-        if(text.length() == desirableLength) return text;
-
-        text = text.trim();
-        String result = "";
-        for(int i = 0; i < desirableLength-text.length();i++){
-            result+="0";
-        }
-        result+=text;
-        return result;
-    }
-
-    //adds spaces after text to desirableLength
-    public static String addSpacesAfter(String text, int desirableLength){
-        if(text.length() > desirableLength) throw new IllegalArgumentException("Text is longer than desirableLength");
-        if(text.length() == desirableLength) return text;
-
-        text = text.trim();
-        String result = text;
-        for(int i = 0; i < desirableLength-text.length();i++){
-            result+=" ";
-        }
-        return result;
     }
 }
